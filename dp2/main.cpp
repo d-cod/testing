84431b28b7117e74faaba1f9fb687aafa60d5aad
Shiva Mahajan <shivamahajan@shivambp.local>
2014-10-02 13:47:25 +0530

    Initial Commit

---
+++ dp2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  dp2
+//
+//  Created by Shiva Mahajan on 02/10/14.
+//  Copyright (c) 2014 Shiva Mahajan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
